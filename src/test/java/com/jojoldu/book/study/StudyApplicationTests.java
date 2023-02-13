8ba0c30775ce6e7d6e047211aeb300c5d7268617
ch-yang1273 <ych1273@gmail.com>
2023-02-13 11:53:17 +0900

    Initial commit

---
+++ src/test/java/com/jojoldu/book/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
